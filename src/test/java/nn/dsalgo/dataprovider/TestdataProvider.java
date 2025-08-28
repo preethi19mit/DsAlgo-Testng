eb6895ccf752b390805167c5b5c5a54313e92eda
sindhuaccet07 <sindhuaccet07@gmail.com>
2025-08-28 16:41:19 -0400

    testdataprovider updated

--- src/test/java/nn/dsalgo/dataprovider/TestdataProvider.java
+++ src/test/java/nn/dsalgo/dataprovider/TestdataProvider.java
@@ -9,7 +9,37 @@ public class TestdataProvider {
                 {"Time Complexity"}
         };
     }
-    
+
+    @DataProvider(name = "inputCodeForTopicsInDS")
+    public Object[][] inputCodeForDSTopics()
+    {
+        return new Object[][] {
+                {"Time Complexity","ValidCode"},
+                {"Time Complexity","InvalidCode"}
+        };
+
+    }
+    @DataProvider(name = "inputCodeForTopicsInTree")
+    public Object[][] inputCodeForTreeTopics()
+    {
+        return new Object[][] {
+                {"Overview of Trees"              ,   "ValidCode"  },
+                {"Terminologies"                  ,   "InvalidCode"},
+                {"Types of Trees"                 ,   "AlphaNumeric"},
+                {"Tree Traversals"                ,   "Numeric"     },
+                {"Traversals-Illustration"        ,   "ValidCode"   },
+                {"Binary Trees"                   ,   "InvalidCode" },
+                {"Types of Binary Trees"          ,   "AlphaNumeric"},
+                {"Implementation in Python"       ,   "Numeric"     },
+                {"Binary Tree Traversals"         ,   "ValidCode"   },
+                {"Implementation of Binary Trees" ,   "InvalidCode" },
+                {"Applications of Binary trees"   ,   "AlphaNumeric"},
+                {"Binary Search Trees"            ,   "Numeric"     },
+                {"Implementation Of BST"          ,   "ValidCode"   }
+        };
+
+    }
+
     @DataProvider(name ="Queuetopics")
     public Object[][] queuetopics()
     {
