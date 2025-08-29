eb640ac1616be84a619f75871873b4d27a1cfead
sindhuaccet07 <sindhuaccet07@gmail.com>
2025-08-28 22:31:59 -0400

    updated

--- src/test/java/nn/dsalgo/hooks/TestNGHooks.java
+++ src/test/java/nn/dsalgo/hooks/TestNGHooks.java
@@ -46,12 +46,6 @@ public class TestNGHooks extends BaseLogger {
     public void logTestStart(Method method) {
         log.info("Starting test: " + method.getName());
     }
-    
-    @AfterMethod(alwaysRun = true)
-    public void browserclose()
-    {
-    	DriverFactory.quitDriver();
-    }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
