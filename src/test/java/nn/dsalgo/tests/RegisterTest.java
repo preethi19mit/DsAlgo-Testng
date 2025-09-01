3ca96c63aacdfabb7edacf3905dd2a9fad23d839
PrasannaRahavi14 <Prasannarahavi14@gmail.com>
2025-09-01 09:47:00 -0400

    Registerandstackprioritygroupadded

--- src/test/java/nn/dsalgo/tests/RegisterTest.java
+++ src/test/java/nn/dsalgo/tests/RegisterTest.java
@@ -9,7 +9,6 @@ import nn.dsalgo.dataprovider.TestdataProvider;
 import nn.dsalgo.factory.DriverFactory;
 import nn.dsalgo.helperclass.HelperClass;
 import nn.dsalgo.hooks.TestNGHooks;
-import nn.dsalgo.listeners.RetryAnalyzer;
 import nn.dsalgo.listeners.TestListeners;
 import nn.dsalgo.pages.RegisterPage;
 
@@ -22,20 +21,22 @@ public class RegisterTest extends TestNGHooks {
   
 	 @BeforeMethod(alwaysRun = true)
 	    public void SetupStackPage() {
+		 
 		 helperclass = new HelperClass();
 		 registerpage = new RegisterPage(DriverFactory.getDriver());
 	    }
-	 @Test
-	    public void getRegisterPage()
-	    {
+	 
+	 @Test(priority=1,groups= {"smoke","sanity","regression"})
+	    public void getRegisterPage() {
+		 
 		 helperclass.RegisterpageLanding();
 		 String ActualTitle = registerpage.ValidateTitleRegisterPage();
 		 Assert.assertEquals(ActualTitle, "Register");
 	     log.info("The user is on page: " + ActualTitle);
 		 }
 	 
-	 @Test(dataProvider = "RegisterTestData",dataProviderClass = TestdataProvider.class, retryAnalyzer = RetryAnalyzer.class)
-	    public void RegisterScenarios(String Testcondition, String ExpectedMessage) {
+	 @Test(priority=2,dataProvider = "RegisterMissingFieldsValidation",dataProviderClass = TestdataProvider.class,groups= {"smoke","regression"})
+	    public void RegisterMissingFieldsValidate(String Testcondition, String ExpectedMessage) {
 		 
 		 helperclass.RegisterpageLanding(); 
 	     String ActualMessage = "";
@@ -62,39 +63,54 @@ public class RegisterTest extends TestNGHooks {
 	                registerpage.getNoPasswordConfirmation();
 	                ActualMessage = registerpage.getPwdConfirmationValidationMessage();
 	            	Assert.assertEquals(ActualMessage, ExpectedMessage);
-	                break;   
-	                
-	            case "MismatchPassword":
-	                registerpage.getMismatchPassword();
-	                ActualMessage = registerpage.getpwdmismatchtext();
-	                Assert.assertEquals(ActualMessage, ExpectedMessage);
-	                break;    
-	                
-	            case "ValidCredentials":
-	                registerpage.getValidCredentials();
-	                ActualMessage = registerpage.getNewAccountAlertmsg();
-	                Assert.assertEquals(ActualMessage, ExpectedMessage);
-	                break;
-	                
+	                break;          
+	            
+	            default:
+	                throw new IllegalArgumentException("Unknown test condition: " + Testcondition);
+		    }
+		 log.info("Actual message: " + ActualMessage);    
+ }
+	         	 
+	 @Test(priority=3,dataProvider = "RegisterValidcredentials",dataProviderClass = TestdataProvider.class,groups= {"smoke","regression"})
+	    public void RegisterValidCredential(String ActualMessage, String ExpectedMessage) {
+		 
+		 helperclass.RegisterpageLanding();
+		 registerpage.getValidCredentials();
+         ActualMessage = registerpage.getNewAccountAlertmsg();
+         Assert.assertEquals(ActualMessage, ExpectedMessage);
+	 }
+	 
+	 @Test(priority=4,dataProvider = "RegisterMismatchPassword",dataProviderClass = TestdataProvider.class,groups= {"sanity","regression"})
+	    public void RegisterMismatchPassword(String ActualMessage, String ExpectedMessage) {
+		 
+		 helperclass.RegisterpageLanding();
+         registerpage.getMismatchPassword();
+         ActualMessage = registerpage.getpwdmismatchtext();
+         Assert.assertEquals(ActualMessage, ExpectedMessage);
+	 }
+
+	 @Test(priority=5,dataProvider = "RegisterInvalidCredentials",dataProviderClass = TestdataProvider.class,groups= {"sanity","regression"})
+	    public void RegisterInvalidCredential(String Testcondition, String ExpectedMessage) {
+		
+		 helperclass.RegisterpageLanding(); 
+	     String ActualMessage = "";
+		 switch (Testcondition) {
 	            case "InvalidUsername":
 	                registerpage.getInvalidUsername();
 	                ActualMessage = registerpage.getpwdmismatchtext();
 	           		Assert.assertEquals(ActualMessage,ExpectedMessage, "Username is not valid");
-	                break;   
+	             break;
 	                
 	            case "PwdNumericValue":
 	                registerpage.getPwdNumericValue();
 	                ActualMessage = registerpage.getpwdmismatchtext();
 	               	Assert.assertEquals(ActualMessage,ExpectedMessage, "Your password can’t be entirely numeric.");
 	                break;
-	                
-	            default:
-	                throw new IllegalArgumentException("Unknown test condition: " + Testcondition);
-		 }
+			 }
 		 log.info("Actual message: " + ActualMessage);
 	 }
 	 
-	 @Test
+	 @Test(priority=6,groups= {"smoke","sanity"})
 	 public void ClickLoginfromRegisterPage()
 	 {
 		 helperclass.RegisterpageLanding();
